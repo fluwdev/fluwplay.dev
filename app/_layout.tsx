afa4a7f518af73b073ca302d9c00faf474b3594b
development-hobby <jesusalcalarojas@gmail.com>
2024-12-18 16:22:49 -0400

    feat: enhance layout with font loading and track player setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,42 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font'
+import { Stack } from 'expo-router'
+import { useSetupTrackPlayer } from '@/hooks/use-setup-track-player'
+import * as SplashScreen from 'expo-splash-screen'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  return <Stack />;
+ useSetupTrackPlayer({
+  onLoad: () => {
+   SplashScreen.hideAsync()
+  },
+ })
+ const [fontsLoaded] = useFonts({
+  'Geist-Regular': require('@/assets/fonts/geist-sans-100.ttf'),
+  'Geist-Medium': require('@/assets/fonts/geist-sans-400.ttf'),
+  'Geist-SemiBold': require('@/assets/fonts/geist-sans-700.ttf'),
+ })
+
+ if (!fontsLoaded) {
+  return null
+ }
+
+ return (
+  <Stack>
+   <Stack.Screen
+    name='player'
+    options={{
+     presentation: 'card',
+     animationMatchesGesture: true,
+     fullScreenGestureEnabled: true,
+     headerShown: false,
+     gestureEnabled: true,
+     animationDuration: 400,
+     gestureDirection: 'vertical',
+     animation: 'slide_from_bottom',
+    }}
+   />
+  </Stack>
+ )
 }
